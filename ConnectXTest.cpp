35a6d90bcb0d7d41c9f0de80d17979973407bbd8
Ashwin Patil <ashwin.s.patil10@gmail.com>
2016-04-07 18:00:19 -0700

    Tested all the functions

--- ConnectXTest.cpp
+++ ConnectXTest.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include "ConnectX.h"
- 
+
 class ConnectXTest : public ::testing::Test
 {
 	protected:
@@ -15,7 +15,77 @@ class ConnectXTest : public ::testing::Test
 		virtual void TearDown(){}
 };
 
+
 TEST(ConnectXTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+
+// Checks at() if the location is out of bound.
+TEST(ConnectXTest, check_at_outOfBound)
+{
+  ConnectX connect;
+  ASSERT_EQ(-1 , (connect.at(-20,30)));
+}
+
+// Checks at() for black player.
+TEST(ConnectXTest, check_at_blackPlayer)
+{
+  ConnectX connect;
+	connect.placePiece(3);
+  ASSERT_EQ(2 , (connect.at(3,5)));
+}
+
+// Checks at() for white player.
+TEST(ConnectXTest, check_at_whitePlayer)
+{
+  ConnectX connect;
+	connect.placePiece(2);
+	connect.placePiece(4);
+  ASSERT_EQ(1 , (connect.at(4,5)));
+}
+
+// Checks at() if the board is empty
+TEST(ConnectXTest, check_at_empty)
+{
+  ConnectX connect;
+  ASSERT_EQ(0 , (connect.at(3,4)));
+}
+
+// Checks whose turn it is.
+TEST(ConnectXTest, check_whoseTurn)
+{
+  ConnectX connect;
+	ASSERT_EQ((connect.whoseTurn()) , 2);
+	connect.placePiece(3);
+	ASSERT_EQ((connect.whoseTurn()),1);
+
+}
+
+// Checks if piece is placed at correct place.
+TEST(ConnectXTest, check_placePiece)
+{
+  ConnectX connect;
+	connect.placePiece(2);
+	ASSERT_EQ(2 , connect.at(2,5));
+
+}
+// Checks if piece is placed out of bounds and wheteher the user is toggeled.
+// Also checks toggelturn()
+TEST(ConnectXTest, check_placePiece_outofbound)
+{
+  ConnectX connect;
+	ASSERT_EQ((connect.whoseTurn()) , 2);
+	connect.placePiece(10);
+	ASSERT_EQ((connect.whoseTurn()) , 1);
+
+}
+
+// Checking constructor for negative value.
+// It should use default set of values and print the map.
+TEST(ConnectXTest, check_constructor)
+{
+	ConnectX connect((-2),(-3),3);
+	connect.showBoard();
 }
